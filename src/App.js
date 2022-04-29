cc47a6dae8e2539e6e1762d898850d470a624c24
Anna Rolfes <anna-rolfes@gmx.de>
2022-04-29 22:38:40 +0200

    Add section to app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import "./App.css";
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Quizz App React</h1>
+      <section>
+        <div class="section__profile">
+          <img
+            class="img--profile"
+            src="/images/clownFishDavidClode.jpg"
+            alt="Larry Lobster"
+          />
+          <figcaption>Larry Lobster</figcaption>
+        </div>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus
+          omnis adipisci distinctio odit maxime quibusdam asperiores rem
+          corporis illo laboriosam possimus quis quos sit, placeat culpa minima
+          dignissimos repudiandae hic?
+        </p>
+      </section>
     </div>
   );
 }
