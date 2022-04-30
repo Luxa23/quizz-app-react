9041a352e83dd43c62b163cd3fdf1630cf419749
Anna Rolfes <anna-rolfes@gmx.de>
2022-04-30 22:23:24 +0200

    Moved button to own component

--- src/App.js
+++ src/App.js
@@ -2,23 +2,7 @@ import './App.css';
 import { items } from './db';
 import { Profile } from './Components/Profile';
 import { MainHeader } from './Components/MainHeader';
-
-function Card({ user }) {
-  return (
-    <section class="card">
-      <h2>{user.title}</h2>
-      <p>{user.question}</p>
-      <button>Show / Hide answer</button>
-
-      <span class="card__answer--hidden">{user.answer}</span>
-      <ul>
-        {user.tags.map(tag => {
-          return <li>{tag}</li>;
-        })}
-      </ul>
-    </section>
-  );
-}
+import { Card } from './Components/Card';
 
 function App() {
   return (
