0910f256d4368610384f9f06427b70f363292a70
Luxa23 <102022862+Luxa23@users.noreply.github.com>
2022-05-02 22:43:21 +0200

    Created styled-components StyledWrapper and StyledSection (#7)

---
+++ src/Components/Card/StyledSection.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const StyledSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  background: rgb(227 202 215);
+  padding-left: 1.5rem;
+  padding-right: 1.5rem;
+  margin: 1rem;
+  box-shadow: 0 2px 11px rgb(25 50 81 / 20%);
+  border-radius: 10px;
+`;
+
+export default StyledSection;
